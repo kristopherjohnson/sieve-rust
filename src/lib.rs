63d3b1d0164e9445fd5c07c9982544a37b374e5c
Kristopher Johnson <kris@kristopherjohnson.net>
2019-04-07 18:35:24 -0400

    Add sieve_iter() which returns iterator rather than a vector

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use std::f64;
 
-/// Return a list of all prime numbers less than or equal to the given value.
-pub fn sieve(n: usize) -> Vec<usize> {
+/// Return a `Vec` of all prime numbers less than or equal to the given value.
+pub fn sieve(max: usize) -> Vec<usize> {
     let mut primes: Vec<usize> = Vec::new();
-    let mut is_prime: Vec<bool> = vec![true; n + 1];
+    let mut is_prime: Vec<bool> = vec![true; max + 1];
 
-    let limit = (n as f64).sqrt() as usize;
-    for p in 2..=n {
+    let limit = (max as f64).sqrt() as usize;
+    for p in 2..=max {
         if !is_prime[p] {
             continue;
         }
@@ -14,7 +14,7 @@ pub fn sieve(n: usize) -> Vec<usize> {
 
         if p <= limit {
             let mut mult = p * p;
-            while mult <= n {
+            while mult <= max {
                 is_prime[mult] = false;
                 mult += p;
             }
@@ -22,6 +22,52 @@ pub fn sieve(n: usize) -> Vec<usize> {
     }
 
     primes
+}
+
+/// Return an iterator that will return prime numbers up to the given maximum.
+pub fn sieve_iter(n: usize) -> Iter {
+    Iter::with_maximum(n)
+}
+
+/// Iterator for prime numbers up to some maximum value.
+pub struct Iter {
+    max: usize,
+    is_prime: Vec<bool>,
+    next_index: usize,
+}
+
+impl Iter {
+    fn with_maximum(max: usize) -> Iter {
+        Iter {
+            max,
+            is_prime: vec![true; max + 1],
+            next_index: 2,
+        }
+    }
+}
+
+impl Iterator for Iter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<usize> {
+        while self.next_index <= self.max && !self.is_prime[self.next_index] {
+            self.next_index += 1;
+        }
+        if self.next_index >= self.max {
+            return None;
+        }
+
+        let p = self.next_index;
+        self.next_index += 1;
+
+        let mut mult = p * p;
+        while mult <= self.max {
+            self.is_prime[mult] = false;
+            mult += p;
+        }
+
+        Some(p)
+    }
 }
 
 #[cfg(test)]
@@ -46,6 +92,26 @@ mod tests {
     #[test]
     fn test_1000() {
         let primes = sieve(1000);
+        assert_eq!(primes.len(), 168);
+        assert_eq!(primes[0], 2);
+        assert_eq!(primes[167], 997);
+    }
+
+    #[test]
+    fn test_iter() {
+        let mut iter = sieve_iter(15);
+        assert_eq!(iter.next().unwrap(), 2);
+        assert_eq!(iter.next().unwrap(), 3);
+        assert_eq!(iter.next().unwrap(), 5);
+        assert_eq!(iter.next().unwrap(), 7);
+        assert_eq!(iter.next().unwrap(), 11);
+        assert_eq!(iter.next().unwrap(), 13);
+        assert!(iter.next().is_none());
+    }
+
+    #[test]
+    fn test_iter_1000() {
+        let primes: Vec<usize> = sieve_iter(1000).collect();
         assert_eq!(primes.len(), 168);
         assert_eq!(primes[0], 2);
         assert_eq!(primes[167], 997);
