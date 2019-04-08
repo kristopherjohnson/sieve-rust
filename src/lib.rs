a2512a8d2d16f6f499992325f2334ee0649a9850
Kristopher Johnson <kris@kristopherjohnson.net>
2019-04-08 18:49:39 -0400

    Expand rustdoc comments for sieve(), sieve_iter(), and sieve::Iter

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
 use std::f64;
 
 /// Return a `Vec` of all prime numbers less than or equal to the given value.
+///
+/// # Example
+///
+/// ```
+/// # use sieve::sieve;
+/// let primes = sieve(100);
+/// assert_eq!(primes.len(), 25);
+/// assert_eq!(primes[0], 2);
+/// assert_eq!(primes[1], 3);
+/// assert_eq!(primes[2], 5);
+/// // ...
+/// assert_eq!(primes[22], 83);
+/// assert_eq!(primes[23], 89);
+/// assert_eq!(primes[24], 97);
+
 pub fn sieve(max: usize) -> Vec<usize> {
     let mut primes: Vec<usize> = Vec::new();
     let mut is_prime: Vec<bool> = vec![true; max + 1];
@@ -25,11 +40,34 @@ pub fn sieve(max: usize) -> Vec<usize> {
 }
 
 /// Return an iterator that will return prime numbers up to the given maximum.
+///
+/// # Example
+///
+/// ```
+/// # use sieve::sieve_iter;
+/// let mut primes = String::from("Primes:");
+/// for prime in sieve_iter(20) {
+///     primes = primes + &format!(" {}", prime);
+/// }
+/// assert_eq!(primes, "Primes: 2 3 5 7 11 13 17 19");
+
 pub fn sieve_iter(max: usize) -> Iter {
     Iter::with_maximum(max)
 }
 
 /// Iterator for prime numbers up to some maximum value.
+///
+/// Returned by the `sieve_iter(max)` function.
+///
+/// # Example
+///
+/// ```
+/// # use sieve::{sieve_iter, Iter};
+/// let mut iter: Iter = sieve_iter(100);
+/// assert_eq!(iter.next(), Some(2));
+/// assert_eq!(iter.next(), Some(3));
+/// assert_eq!(iter.next(), Some(5));
+
 pub struct Iter {
     max: usize,
     is_prime: Vec<bool>,
