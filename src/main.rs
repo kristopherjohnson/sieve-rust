9997ad5348a1a33ae0d43b200e13646b715ff765
Kristopher Johnson <kris@kristopherjohnson.net>
2019-04-10 20:42:28 -0400

    Allow max to be specified on command line for main()

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
-use sieve::sieve;
+use sieve::sieve_iter;
+
+use std::env;
 
 fn main() {
-    let n = 100;
-    let primes = sieve(n);
-    println!("Found {} primes <= {}:", primes.len(), n);
-    for prime in primes {
+    let mut max = 100;
+
+    let args = env::args().collect::<Vec<String>>();
+    if args.len() == 2 {
+        max = usize::from_str_radix(&args[1], 10).unwrap();
+    }
+
+    let mut count = 0;
+    for prime in sieve_iter(max) {
         print!("{} ", prime);
+        count += 1;
     }
     println!();
+    println!("Count of primes 2-{}: {}", max, count);
 }
