de64d5e8a0fdc2b4f30c41e4e63fab834ca8a85a
Kristopher Johnson <kris@kristopherjohnson.net>
2019-04-12 09:18:57 -0400

    Provide better error messages for invalid command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,29 @@ use std::process;
 
 const DEFAULT_MAX: usize = 1000;
 
+fn print_help_and_exit(program_name: &str) -> ! {
+    eprintln!("usage: {} [MAX]", program_name);
+    eprintln!("       Default maximum is {}.", DEFAULT_MAX);
+    process::exit(-1)
+}
+
 fn main() {
     let args = env::args().collect::<Vec<String>>();
 
     // Optional command-line argument is maximum value.
     let max = if args.len() == 2 {
-        args[1].parse().unwrap()
+        match args[1].parse() {
+            Ok(n) => n,
+            Err(_) => {
+                eprintln!("error: invalid argument: \"{}\"", &args[1]);
+                print_help_and_exit(&args[0])
+            }
+        }
     } else if args.len() == 1 {
         DEFAULT_MAX
     } else {
-        eprintln!("usage: {} [MAX]", args[0]);
-        eprintln!("       Default maximum is {}.", DEFAULT_MAX);
-        process::exit(-1);
+        eprintln!("error: too many arguments");
+        print_help_and_exit(&args[0])
     };
 
     let mut count = 0;
