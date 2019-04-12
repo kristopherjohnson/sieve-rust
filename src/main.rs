a88869e69a9d5beb3beca6911f994d85a7de4c49
Kristopher Johnson <kris@kristopherjohnson.net>
2019-04-12 08:59:55 -0400

    Refactor command-line argument handling in main(); change default max from 100 to 1000

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,21 @@ use sieve::sieve_iter;
 use std::env;
 use std::process;
 
-fn main() {
-    let mut max = 100;
+const DEFAULT_MAX: usize = 1000;
 
+fn main() {
     let args = env::args().collect::<Vec<String>>();
-    if args.len() == 2 {
-        max = args[1].parse().unwrap();
-    } else if args.len() != 1 {
+
+    // Optional command-line argument is maximum value.
+    let max = if args.len() == 2 {
+        args[1].parse().unwrap()
+    } else if args.len() == 1 {
+        DEFAULT_MAX
+    } else {
         eprintln!("usage: {} [MAX]", args[0]);
-        eprintln!("       Default maximum is {}.", max);
+        eprintln!("       Default maximum is {}.", DEFAULT_MAX);
         process::exit(-1);
-    }
+    };
 
     let mut count = 0;
     for prime in sieve_iter(max) {
